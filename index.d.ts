484721e3d72e0ea8a2e8f59f9b6d47da90ecbbd2
Krzysztof Saczuk <zakuciael@outlook.com>
2018-10-16 09:03:27 +0200

    Implemented Logging Levels, Saving logs to file, Configurable singleton model.

--- index.d.ts
+++ index.d.ts
@@ -1,25 +1,44 @@
-declare module "colored-logs" {
+import {WriteStream} from "fs";
 
+declare module "colored-logs" {
     enum Level {
+        ERROR = "error",
+        WARN = "warn",
         INFO = "info",
         DEBUG = "debug",
-        WARN = "warn",
-        ERROR = "error"
+    }
+
+    interface LoggerOptions {
+        level?: string;
+        filePath?: string;
+        timezone?: string;
+        timed_logs?: boolean;
     }
 
     class Logger {
-        public static timezone: string;
         public static readonly LEVELS: Level;
+        public static readonly options: LoggerOptions;
+        private static fileWriteStream: WriteStream | undefined;
+
+        public readonly LEVELS: Level;
+        public readonly options: LoggerOptions;
+        private fileWriteStream: WriteStream | undefined;
+
+        constructor();
+        constructor(opt: LoggerOptions);
 
         public static log(level: Level, ...messages: any[]): void;
+        public log(level: Level, ...messages: any[]): void;
 
         public static info(...messages: any[]): void;
         public static debug(...messages: any[]): void;
         public static warn(...messages: any[]): void;
-        public static error(error: Error, when?: string): void;
+        public static error(error: Error | string, when?: string): void;
 
-        private static logToConsole(title: string, messages: string[]): void;
-        private static createLogFile(err: Error): void;
+        public info(...messages: any[]): void;
+        public debug(...messages: any[]): void;
+        public warn(...messages: any[]): void;
+        public error(error: Error | string, when?: string): void;
     }
 
     export {Logger};
--- index.ts
+++ index.ts
@@ -1,98 +1,135 @@
 import prettyFormat from "pretty-format";
 import Time from "timezone-utils";
-import chalk from "chalk";
 import path from "path";
 import util from "util";
-import fs from "fs";
+import fs, {WriteStream} from "fs";
+import chalk from "chalk";
 
 enum Level {
+    ERROR = "error",
+    WARN = "warn",
     INFO = "info",
     DEBUG = "debug",
-    WARN = "warn",
-    ERROR = "error"
 }
 
-function indentText(str: string) {
-    return str.replace(/^(?!\s+$)/gm, " ".repeat(13)).trim();
+interface LoggerOptions {
+    level?: string;
+    filePath?: string;
+    timezone?: string;
+    timed_logs?: boolean;
 }
-function debugEnabled() {
-    return process.env.DEBUG ? process.env.DEBUG.trim() !== "false" : false;
-}
+
+const options: LoggerOptions = {
+    level: process.env.LOG_LEVEL || (process.env.DEBUG ? Level.DEBUG : Level.INFO),
+    timezone: new Time().timezone,
+    filePath: undefined,
+    timed_logs: true
+};
 
 class Logger {
-    public static timezone: string = "Europe/Warsaw";
     public static readonly LEVELS = Level;
+    public static readonly options = options;
+    private static fileWriteStream: WriteStream | undefined;
 
-    public static log(level: Level, ...messages: any[]): void {
-        if (eval(`Logger.${level}`)) {
-            eval(`Logger.${level}`).apply(this, messages);
-        } else {
-            this.error(new Error("Unable to find logger level: " + level.toUpperCase()), "creating log message");
+    public readonly LEVELS = Level;
+    public readonly options = options;
+    private fileWriteStream: WriteStream | undefined;
+
+    constructor(opt: LoggerOptions = {}) {
+        if (Object.values(this.LEVELS).indexOf(opt.level) === -1) opt.level = this.LEVELS.INFO;
+        if (opt.filePath) opt.timed_logs = false;
+        Object.assign(options, opt);
+    }
+
+    /* Main Method */
+    public static log(level: Level, ...messages: any[]) {
+        if (Object.values(this.LEVELS).indexOf(level) > Object.values(this.LEVELS).indexOf(this.options.level)) return;
+
+        const titles: { [key: string]: string } = {};
+        titles[this.LEVELS.ERROR] = chalk.red(this.LEVELS.ERROR.toUpperCase());
+        titles[this.LEVELS.WARN] = chalk.yellow(this.LEVELS.WARN.toUpperCase());
+        titles[this.LEVELS.INFO] = chalk.blue(this.LEVELS.INFO.toUpperCase());
+        titles[this.LEVELS.DEBUG] = chalk.yellowBright(this.LEVELS.DEBUG.toUpperCase());
+
+        let error: Error | undefined;
+        if (messages[1] instanceof Error && level === this.LEVELS.ERROR) {
+            error = messages[1];
+            messages = messages.slice(0, 1);
+        }
+
+        const formatted = messages.map((m) => {
+            if (typeof m === "string") return m;
+
+            return prettyFormat(m, {
+                highlight: true,
+                min: true,
+                theme: {
+                    boolean: "yellow",
+                    bracket: "grey",
+                    comma: "grey",
+                    content: "reset",
+                    date: "green",
+                    error: "red",
+                    function: "blue",
+                    key: "cyan",
+                    label: "blue",
+                    misc: "grey",
+                    number: "green",
+                    prop: "yellow",
+                    regex: "red",
+                    string: "reset",
+                    symbol: "red",
+                    tag: "cyan",
+                    value: "green",
+                },
+            });
+        }).map((m) => m.replace(/^(?!\s+$)/gm, " ".repeat(13)).trim());
+
+        if (!this.fileWriteStream) {
+            if (this.options.filePath && !this.options.timed_logs) {
+
+                let i = 1;
+                while (fs.existsSync(path.resolve(this.options.filePath))) {
+                    let dir = path.dirname(this.options.filePath);
+                    let fileName = path.basename(this.options.filePath).split(".").shift();
+                    let ext = path.extname(this.options.filePath);
+                    this.options.filePath = `${dir}/${fileName ? fileName.replace(/-\d+/g, "") : ""}-${i}${ext}`;
+                    i++;
+                }
+
+                this.fileWriteStream = fs.createWriteStream(path.resolve(this.options.filePath));
+            } else {
+                let time = new Time(this.options.timezone);
+                if (!fs.existsSync(path.resolve("./logs"))) fs.mkdirSync(path.resolve("./logs"));
+                this.fileWriteStream = fs.createWriteStream(path.resolve(`./logs/${time.date()} ${time.format("HH-mm-ss")}.log`));
+            }
+        }
+
+        process.stdout.write(util.format.apply(arguments, [chalk.grey(new Time(this.options.timezone).time()), `[${titles[level]}]`, ...formatted]) + "\n");
+        this.fileWriteStream.write(`${new Time(this.options.timezone).time()} [${level.toUpperCase()}] ${formatted.join(" ")}\n`);
+
+        if (error) {
+            process.stdout.write(util.format.apply(arguments, [error]) + "\n");
+            this.fileWriteStream.write(error.stack + "\n");
         }
     }
 
-    public static info(...messages: any[]): void {
-        this.logToConsole(chalk.blue("INFO"), messages);
-    }
+    /* Static Methods */
+    public static info = (...messages: any[]) => Logger.log(Level.INFO, ...messages);
+    public static debug = (...messages: any[]) => Logger.log(Level.DEBUG, ...messages);
+    public static warn = (...messages: any[]) => Logger.log(Level.WARN, ...messages);
+    public static error = (error: Error | string, when?: string) => {
+        if (typeof error === "string") error = new Error(error);
 
-    public static warn(...messages: any[]): void {
-        this.logToConsole(chalk.yellow("WARNING"), messages);
-    }
+        return Logger.log(Level.ERROR, `An error occurred while ${when ? when : "doing important stuff..."}`, error);
+    };
 
-    public static error(error: Error, when?: string): void {
-        this.createLogFile(error);
-        this.logToConsole(chalk.red("ERROR"), [`An error occurred while ${when ? when : "doing important stuff.."}`]);
-    }
-
-    public static debug(...messages: any[]): void {
-        if (!debugEnabled()) return;
-        this.logToConsole(chalk.yellowBright("DEBUG"), messages);
-    }
-
-    private static logToConsole(title: string, messages: string[]): void {
-        const formattedMessage = messages
-            .map((message) => {
-                // noinspection SuspiciousTypeOfGuard
-                if (typeof message === "string") {
-                    return message;
-                }
-
-                return prettyFormat(message, {
-                    highlight: true,
-                    min: true,
-                    theme: {
-                        boolean: "yellow",
-                        bracket: "grey",
-                        comma: "grey",
-                        content: "reset",
-                        date: "green",
-                        error: "red",
-                        function: "blue",
-                        key: "cyan",
-                        label: "blue",
-                        misc: "grey",
-                        number: "green",
-                        prop: "yellow",
-                        regex: "red",
-                        string: "reset",
-                        symbol: "red",
-                        tag: "cyan",
-                        value: "green",
-                    },
-                });
-            })
-            .map(indentText);
-
-        process.stdout.write(util.format.apply(arguments, [chalk.grey(new Time(this.timezone).time()), `[${title}]`, ...formattedMessage]) + "\n");
-    }
-
-    private static createLogFile(err: Error): void {
-        if (!fs.existsSync(path.resolve("logs")))
-            fs.mkdirSync(path.resolve("logs"));
-
-        const time = new Time(this.timezone);
-        fs.writeFileSync(path.resolve("logs",  `${time.date()} ${time.format("HH-mm-ss")}.log`), err.stack);
-    }
+    /* Non-Static Methods */
+    public log = (level: Level, ...messages: any[]) => Logger.log(level, ...messages);
+    public info = (...messages: any[]) => Logger.log(Level.INFO, ...messages);
+    public debug = (...messages: any[]) => Logger.log(Level.DEBUG, ...messages);
+    public warn = (...messages: any[]) => Logger.log(Level.WARN, ...messages);
+    public error = (error: Error | string, when?: string) => Logger.error(error, when);
 }
 
 export {Logger};
